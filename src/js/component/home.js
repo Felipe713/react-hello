bd93c9f5c4be95d845f1b11567089b06000a64d9
Felipe713 <felipeivp@gmail.com>
2021-06-18 00:16:48 +0000

    semaforo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("green");
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div id="trafficTop"></div>
+			<div id="container">
+				<div
+					className={`red $(color === === "red" ? "light" : "") `}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={`yellow $(color === === "yellow" ? "light" : "") `}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={`green $(color === === "green" ? "light" : "") `}
+					onClick={() => setColor("green")}></div>
+			</div>
 		</div>
 	);
 }
